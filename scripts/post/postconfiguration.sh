26e27466970d33f6bee3aaf173b88d72864c8357
odintdh <odintdh@users.noreply.github.com>
2023-06-01 20:00:39 +0200

    disable blackbox

--- scripts/post/postconfiguration.sh
+++ scripts/post/postconfiguration.sh
@@ -32,5 +32,5 @@ install -o root -g root -m 0755 mc /usr/bin/mc
 #gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
 #sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf
 #sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf
-flatpak install flathub com.raggesilver.BlackBox
+#flatpak install flathub com.raggesilver.BlackBox
 #systemctl unmask dconf-update.service && systemctl enable dconf-update.service
