548dcc6afdc56a625bef77559a94155c76f760cf
Alberto <odintdh@gmail.com>
2023-06-06 11:00:15 +0200

    feat: added postgresql admin repo

--- scripts/pre/preconfiguration.sh
+++ scripts/pre/preconfiguration.sh
@@ -7,3 +7,4 @@ set -oue pipefail
 
 # Your code goes here.
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=0\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
+rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-fedora-repo-2-1.noarch.rpm
