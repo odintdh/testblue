37d6e062b607b9904a83445d05c913644be7c138
Alberto <odintdh@gmail.com>
2023-06-06 13:17:56 +0200

    feat: added mysql repo

--- scripts/pre/preconfiguration.sh
+++ scripts/pre/preconfiguration.sh
@@ -7,4 +7,5 @@ set -oue pipefail
 
 # Your code goes here.
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=0\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
+rpm -i https://dev.mysql.com/get/mysql80-community-release-fc38-1.noarch.rpm
 rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-fedora-repo-2-1.noarch.rpm
